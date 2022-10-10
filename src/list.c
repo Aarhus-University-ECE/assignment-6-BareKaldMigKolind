eda8a705d4f5dfba97e8d6b9fe80aeb95d718694
BareKaldMigKolind <madsskolind@gmail.com>
2022-10-10 15:45:08 +0200

    Lavet aflevering

--- src/list.c
+++ src/list.c
@@ -21,28 +21,73 @@ void add(node *head, int x){
   p->next = element; 
 }
 
-int size(node *l){
+int size(node *head){
     // Excercise 3b)
-    // Add your code here... 
+    // Først tjekker jeg at mit head ikke er null, altså at der er "rigtige" elementer
+    assert(head!=NULL);
+    //Dernæst definerer jeg en pointer p, som peger på head i en node.
+    node *p = head;
 
-    return -1;
+    //Her definerer jeg en variabel, som skal agere counter. 
+    int size = 0;
+
+    //Jeg laver nu en while løkke, som skal køre så længe p->next != NULL. Altså det skal gerne køre til det sidste element i rækken. 
+    //For hver runde lægger jeg en til size. Dermed kan jeg tælle antallet af elementer i arrayet. 
+    while (p->next!= NULL) {
+      size++; 
+      //Jeg opdaterer pointeren til at være det som pointeren peger på. Dermed kommer jeg et element frem i rækken.  
+      p = p->next;
+    }
+    //Her er min gennemkørsel færdig og jeg kan returne size. 
+    return size;
 }
 
-void printout(node *l) {
+void printout(node *head) {
   /*Excercise 3d) Implement your changes.. 
     pre: head points to the first, empty element. The last element's next is NULL
     post: the values of the list are printed out*/
-    node *p = l->next;
+    
+    //Her sørger jeg for at mine preconditions holder. At det sidste elements next er NULL, "tjekkes" af min while løkke længere nede. 
+    assert(head != NULL);
+
+    //Jeg definerer en pointer p til at pege på heads next. Altså på det første "rigtige" element. 
+    node *p = head->next;
+
+    //Her kører en while løkke så længe p != NULL altså skal den gerne køre indtil sidste element i rækken. 
     while (p!=NULL){
+      //For hver gennemgang printer jeg p->data. og opdaterer p til at være p->next. Dermed kommer jeg til næste element i rækken. 
       printf("%d, ",p->data);
+      p = p->next;
     }
     printf("\n");
 }
 
-int largest(node *l){
+int largest(node *head){
     /*Excercise 3e) Add your code below.
-      pre: head points to the first, empty element. The last element's next is NULL. size(l>0)
+      pre: head points to the first, empty element. The last element's next is NULL. size(head>0)
       post: returns the largest value of the list*/
-    return -1; 
+      
+    //Igen tjekkes mine preconditions med en assert. Her tjekker jeg at head != NULL og at størrelsen af head > 0. 
+    assert(head!=NULL && size(head)>0);
+
+    //Her definerer jeg en pointer til at pege på heads -> next. Altså det første "rigtige" element i rækken. 
+    node *p = head -> next;
+
+    //Jeg laver en variabel, som skal være min største værdi i rækken. I starten må det være det første rigtige elements værdi. 
+    int largest = p->data;
+
+    //Her laver jeg en while, som skal køre så længe p != NULL. Altså så længe next i structen ikke er NULL. 
+    while (p != NULL) {
+
+      //Hvis data i gennemgangen er større end largest, så gemmer jeg den værdi, som den nye largest. 
+      if(p->data > largest) {
+        largest = p->data;
+      }
+
+      //Her opdaterer jeg p, så vi kommer igennem rækken af elementer. 
+      p = p -> next; 
+    }
+
+    return largest; 
 }
 
--- src/max.c
+++ src/max.c
@@ -5,6 +5,18 @@ int max(int* numbers, int size)
 {
     // Excercise 2
     // Implement your code below...
+    //Laver en variabel max til at gemme en max værdi i. 
+    assert (size > 0);
 
-    return -1;
+    int max = numbers[0];
+
+    //Laver et for-loop som kører arrayet igennem. Hvis en værdi number[i] er større end max, 
+    //så bliver det den nye max. Dermed ender jeg med den største værdi i arrayet. 
+    for (int i = 0; i < size; i++) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+
+    return max;
 }
